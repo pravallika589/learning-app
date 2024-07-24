0cb4dcd80a49ffc3c5e23664c9b240d5cf572de6
PRAVALLIKA M <pravallikam.1996@gmail.com>
2024-07-24 15:45:48 -0500

    routing changes

---
+++ src/app/vehicle/vehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+
+
+@Component({
+  selector: 'app-vehicle',
+  templateUrl: './vehicle.component.html',
+  styleUrl: './vehicle.component.css'
+})
+export class VehicleComponent {
+
+
+
+}
+
+
